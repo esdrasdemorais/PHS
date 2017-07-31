2ca32213fada6f4e6b4e29ebb73dc354ecac5473
Victor Hugo Garcia Caetano <victorcaetano@proguaru.sa>
2017-07-31 20:40:06 -0300

    Implementação bootstrap

--- _app/view/cabecalho.tpl.php
+++ _app/view/cabecalho.tpl.php
@@ -5,10 +5,13 @@
         <meta charset="UTF-8">
         <title>#title#</title>
         <link rel="stylesheet" href="#url#/css/default.css" />
+        <link rel="stylesheet" href="#url#/css/bootstrap.min.css" />
         <script type="text/javascript" src="#url#/js/jquery.js"></script>
+        <script type="text/javascript" src="#url#/js/bootstrap.min.js"></script>
         <!--[if lt IE 9]>
         <script src="/js/html5shiv.js"></script>
         <![endif]-->
         #includes#     
     </head>
     <body>
+        <div class="container">
--- _app/view/cliente/salvar.tpl.php
+++ _app/view/cliente/salvar.tpl.php
@@ -1,33 +1,42 @@
+<style type="text/css">
+    .form-group { color:#337ab7 !important; }
+</style>
 <form method="post" title="CadastroCliente" id="cad_cliente"
     action="#url#/index.php/cliente/salvar">
     <input type="hidden" name="id" value="#id#" />
-    <label>Nome:</label>
-    <input type="text" name="cli_nome" maxlength="170" value="#nome#" 
-        title="Informe o nome completo." min="17"
-        x-moz-errormessage="Informe o nome completo." required />
-    <br>
-    <label>E-mail:</label>
-    <input type="email" name="cli_email" maxlength="170" value="#email#" 
-        required title="Informe um email válido." 
-        x-moz-errormessage="Informe um email válido." />
-    <br>
-    <label>Telefone Celular:</label>
-    <input type="tel" name="cli_telefone" maxlength="170" value="#telefone#" 
-        placeholder="ddd(11)numero(999999999)" requery 
-        pattern="^\d{10,11}$" required maxlength="11" 
-        title="Informe o telefone somente com número" 
-        x-moz-errormessage="Informe o telefone somente com número" />
-    <br>
-    <label>Endereço:</label>
-    <input type="search" name="cli_endereco" id="endereco" value="#endereco#" 
-        placeholder="rua, número, bairro, cidade, sigla estado, cep" required 
-        title="Informe o endereço" x-moz-errormessage="Informe o endereço" />
-    <section id="place" 
-        style="min-width:280px;display:none;border:1px solid #000;cursor:pointer;
-        text-decoration:underline">
-    </section>
+    <div class="form-group">
+        <label>Nome:</label>
+        <input type="text" name="cli_nome" maxlength="170" value="#nome#" 
+            title="Informe o nome completo." min="17" class="form-control"
+            x-moz-errormessage="Informe o nome completo." required />
+    </div>
+    <div class="form-group">
+        <label>E-mail:</label>
+        <input type="email" name="cli_email" maxlength="170" value="#email#" 
+            required title="Informe um email válido." class="form-control"
+            x-moz-errormessage="Informe um email válido." />
+    </div>
+    <div class="form-group">
+        <label>Telefone Celular:</label>
+        <input type="tel" name="cli_telefone" maxlength="170" value="#telefone#" 
+            placeholder="ddd(11)numero(999999999)" requery 
+            pattern="^\d{10,11}$" required maxlength="11" 
+            title="Informe o telefone somente com número" class="form-control" 
+            x-moz-errormessage="Informe o telefone somente com número" />
+    </div>
+    <div class="form-group">
+        <label>Endereço:</label>
+        <input type="search" name="cli_endereco" id="endereco" value="#endereco#" 
+            placeholder="rua, número, bairro, cidade, sigla estado, cep" required 
+            title="Informe o endereço" x-moz-errormessage="Informe o endereço"
+            class="form-control" />
+        <section id="place" 
+            style="min-width:280px;display:none;border:1px solid #000;cursor:pointer;
+            text-decoration:underline">
+        </section>
+    </div>
     <input type="hidden" name="endereco_id" id="endereco_id" value="" />
     <input type="hidden" name="geolocalizacao" id="geolocalizacao" value="" />
     <br>
-    <input type="submit" name="salvar" value="Salvar" />
+    <input type="submit" name="salvar" value="Salvar" class="btn btn-primary" />
 </form>
